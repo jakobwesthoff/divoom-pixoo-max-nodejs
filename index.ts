e00d2d915163050ff06b23c459a7ffe4162bdeaa
Jakob Westhoff <jakob@westhoffswelt.de>
2022-01-16 16:09:54 +0100

    First PixooMax comaptible image write

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { BluetoothSerialPort } from "node-bluetooth-serial-port";
+import { Grid, RgbColor } from "./src/Grid";
 
 function calculateChecksum(payloadWithLength: Buffer): Buffer {
   let sum = 0;
@@ -27,34 +28,6 @@ function delay(timeout: number): Promise<void> {
   return new Promise<void>((resolve) => setTimeout(resolve, timeout));
 }
 
-// function PError(
-//   fn: (...args: unknown[]) => unknown
-// ): (...args: unknown[]) => Promise<unknown> {
-//   return (...args: unknown[]) =>
-//     new Promise((resolve, reject) =>
-//       fn(...args, (error: unknown, ...data: unknown[]) => {
-//         if (error) {
-//           reject(error);
-//         } else {
-//           resolve(data);
-//         }
-//       })
-//     );
-// }
-
-// function P(
-//   fn: (...args: unknown[]) => unknown
-// ): (...args: unknown[]) => Promise<unknown> {
-//   return (...args: unknown[]) =>
-//     new Promise((resolve, reject) =>
-//       fn(
-//         ...args,
-//         (...data: unknown[]) => resolve(data),
-//         (error: Error) => reject(error)
-//       )
-//     );
-// }
-
 async function sendPayload(
   bt: BluetoothSerialPort,
   payload: Buffer
@@ -64,7 +37,52 @@ async function sendPayload(
   await new Promise<void>((resolve, reject) =>
     bt.write(msg, (error?: Error) => (error ? reject(error) : resolve()))
   );
-  console.log("<<< ", msg);
+  console.log("<<<", msg.toString("hex"));
+}
+
+function encodeFrame(grid: Grid): Buffer {
+  const { imageBuffer, paletteSizeLE } = grid.toImageData();
+  const frameSize = Buffer.alloc(2);
+  frameSize.writeUInt16LE(8 + imageBuffer.length);
+
+  return Buffer.concat([
+    Buffer.from([0xaa]),
+    frameSize,
+    Buffer.from([0x00, 0x00, 0x03]),
+    paletteSizeLE,
+    imageBuffer,
+  ]);
+}
+
+function encodeAnimationFrame(image: Buffer): Buffer {
+  const lengthBuffer = Buffer.alloc(2);
+  lengthBuffer.writeUInt16LE(image.length + 3);
+  return Buffer.concat([Buffer.from([0xaa]), lengthBuffer, image]);
+}
+
+function encodeAnimationChunk(
+  encodedFrame: Buffer,
+  index: number,
+  chunk: Buffer
+): Buffer {
+  const lengthBuffer = Buffer.alloc(2);
+  lengthBuffer.writeUInt16LE(encodedFrame.length);
+  return Buffer.concat([lengthBuffer, Buffer.from([index]), chunk]);
+}
+
+function createAnimationFrame(
+  timeCode: number,
+  paletteSizeLE: Buffer,
+  imageData: Buffer
+): Buffer {
+  const timeCodeLE = Buffer.alloc(2);
+  timeCodeLE.writeUInt16LE(timeCode);
+  return Buffer.concat([
+    timeCodeLE,
+    Buffer.from([0x00]),
+    paletteSizeLE,
+    imageData,
+  ]);
 }
 
 (async () => {
@@ -75,7 +93,7 @@ async function sendPayload(
     const bt = new BluetoothSerialPort();
 
     bt.on("error", (error) => console.error("Error: ", error));
-    bt.on("data", (data: Buffer) => console.log(">>> ", data));
+    bt.on("data", (data: Buffer) => console.log(">>>", data.toString("hex")));
 
     console.log(`Searching for COM channel on ${HCI_ADDRESS}...`);
     const channel = await new Promise<number>((resolve, reject) =>
@@ -89,13 +107,132 @@ async function sendPayload(
     );
     console.log("connected");
 
+    // DO NOT REMOVE
+    //
+    // We need to wait a little after connecting before talking to the display,
+    // as otherwise our cammands are ignored or only read partially.
+    // 100ms seems to work. No idea what the minimum here would be.
+    //
+    // DO NOT REMOVE
     await delay(100);
 
-    for (let i = 0; i <= 100; i += 10) {
-      await sendPayload(bt, Buffer.from([0x74, i]));
-      await delay(250);
+    /*
+     * Brightness Change
+     * Command: 0x74
+     * Args: 0x00 - 0x64 (1 byte) (somehow stepped)
+     */
+    // for (let i = 0; i <= 100; i += 10) {
+    //   await sendPayload(bt, Buffer.from([0x74, i]));
+    //   await delay(250);
+    // }
+    await sendPayload(bt, Buffer.from([0x74, 0x64]));
+
+    // Playing with channel switching
+    // await sendPayload(bt, Buffer.from([0x74, 0x64]));
+    // await delay(1000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x00, 0x00, 0x01, 0x00])); // Clock?
+    // await delay(5000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x00, 0x00, 0x01, 0x01])); // Clock?
+    // await delay(5000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x00, 0x00, 0x01, 0x02])); // Clock?
+    // await delay(5000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x00, 0x00, 0x01, 0x03])); // Clock?
+    // await delay(5000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00])); // Clock? / No real idea!
+    // await delay(5000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x00, 0x00, 0x01, 0x05])); // Clock?
+    // await delay(2000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x01, 0xff, 0xff, 0x00, 0x64, 0x00, 0x01, 0x00, 0x00, 0x00])); // Lightning (see documentation)
+    // await delay(2000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x02])); // Cloud Channel
+    // await delay(2000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x03])); // No real idea some random animations
+    // await delay(2000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x04])); // Visualization (see documentation)
+    // await delay(2000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x06]));
+    // await delay(2000);
+    // await sendPayload(bt, Buffer.from([0x45, 0x07]));
+    // await delay(2000);
+
+    let iteration = 0;
+    while (true) {
+      const grid = new Grid();
+      const colors: RgbColor[] = [
+        [255, 255, 255],
+        // [0, 0, 0],
+        // [255, 0, 0],
+        // [0, 255, 0],
+        // [0, 0, 255],
+        // [255, 255, 0],
+        [255, 0, 255],
+        // [0, 255, 255],
+      ];
+      // let currentColor = iteration++ % colors.length;
+      let currentColor = 0;
+      let index = 0;
+
+      grid.map((_x, _y, color) => {
+        if (index > iteration) {
+          return color;
+        }
+
+        let newColor = currentColor;
+        currentColor = (currentColor + 1) % colors.length;
+
+        index++;
+
+        return colors[newColor];
+      });
+
+      await sendPayload(
+        bt,
+        Buffer.concat([
+          Buffer.from([0x44, 0x00, 0x0a, 0x0a, 0x04]),
+          encodeFrame(grid),
+        ])
+      );
+
+      iteration++;
+      await delay(16);
     }
 
+    // ANIMATION TRYOUT
+    // const allLength = Buffer.alloc(2);
+    // allLength.writeUInt16LE(6 + imageBuffer.length);
+
+    // const newImageBufferLength = Buffer.alloc(2);
+    // allLength.writeUInt16LE(3 + imageBuffer.length);
+
+    // let chunkSize = 200;
+    // let start = 0;
+    // let chunk = imageBuffer.slice(
+    //   start,
+    //   Math.min(imageBuffer.length, start + chunkSize)
+    // );
+    // let chunkIndex = 0;
+
+    // let aframe = encodeAnimationFrame(
+    //   createAnimationFrame(100, paletteSizeLE, imageBuffer)
+    // );
+
+    // const animationFrame = Buffer.concat([aframe, aframe]);
+
+    // while (start < animationFrame.length) {
+    //   await sendPayload(
+    //     bt,
+    //     Buffer.concat([
+    //       Buffer.from([0x49]),
+    //       encodeAnimationChunk(animationFrame, chunkIndex++, chunk),
+    //     ])
+    //   );
+
+    //   start += chunkSize;
+    //   chunk = animationFrame.slice(
+    //     start,
+    //     Math.min(animationFrame.length, start + chunkSize)
+    //   );
+    // }
   } catch (error) {
     console.error("Main error: ", error);
     process.exit(1);
---
+++ src/Grid.ts
@@ -0,0 +1,178 @@
+import { getCipherInfo } from "crypto";
+
+export type RgbColor = [number, number, number];
+
+export type ImageData = {
+  paletteBuffer: Buffer;
+  screenBuffer: Buffer;
+  imageBuffer: Buffer;
+  palette: RgbColor[];
+  screen: number[];
+  paletteSizeLE: Buffer;
+  screenBufferSizeLE: Buffer;
+  imageBufferSizeLE: Buffer;
+};
+
+export class Grid {
+  public static WIDTH = 32;
+  public static HEIGHT = 32;
+
+  private pixels: RgbColor[] = [];
+
+  public constructor() {
+    this.map(() => [0, 0, 0]);
+  }
+
+  public forEach(fn: (x: number, y: number, color: RgbColor) => void) {
+    for (let y = 0; y < Grid.HEIGHT; y++) {
+      for (let x = 0; x < Grid.WIDTH; x++) {
+        fn(x, y, this.pixels[y * Grid.WIDTH + x]);
+      }
+    }
+  }
+
+  public map(fn: (x: number, y: number, color: RgbColor) => RgbColor) {
+    for (let y = 0; y < Grid.HEIGHT; y++) {
+      for (let x = 0; x < Grid.WIDTH; x++) {
+        this.pixels[y * Grid.WIDTH + x] = fn(
+          x,
+          y,
+          this.pixels[y * Grid.WIDTH + x]
+        );
+      }
+    }
+  }
+
+  private assertBounds(x: number, y: number) {
+    if (x < 0 || x > Grid.WIDTH) {
+      throw new Error(`x coordinate out of bounds: ${x} > ${Grid.WIDTH}`);
+    }
+
+    if (y < 0 || y > Grid.HEIGHT) {
+      throw new Error(`y coordinate out of bounds: ${y} > ${Grid.HEIGHT}`);
+    }
+  }
+
+  public set(x: number, y: number, color: RgbColor): RgbColor {
+    this.assertBounds(x, y);
+    const oldValue = this.pixels[y * Grid.WIDTH + x];
+    this.pixels[y * Grid.WIDTH + x] = color;
+    return oldValue;
+  }
+
+  public get(x: number, y: number): RgbColor {
+    this.assertBounds(x, y);
+    return this.pixels[y * Grid.WIDTH + x];
+  }
+
+  public toImageData(): ImageData {
+    const palette: RgbColor[] = [];
+    const paletteIndexMap = new Map<string, number>();
+
+    const screen: number[] = [];
+
+    this.forEach((x, y, color) => {
+      const stringifiedColor = JSON.stringify(color);
+      if (!paletteIndexMap.has(stringifiedColor)) {
+        palette.push(color);
+        const index = palette.length - 1;
+        paletteIndexMap.set(stringifiedColor, index);
+      }
+      screen.push(paletteIndexMap.get(stringifiedColor)!);
+    });
+
+    // We should ideally dither the image in this case.
+    if (palette.length > 256) {
+      throw new Error(
+        `Palette to large: More than 256 colors (${palette.length})`
+      );
+    }
+
+    const paletteBuffer = Buffer.alloc(palette.length * 3);
+    palette.forEach((color, index) => {
+      paletteBuffer.writeUInt8(color[0], index * 3 + 0);
+      paletteBuffer.writeUInt8(color[1], index * 3 + 1);
+      paletteBuffer.writeUInt8(color[2], index * 3 + 2);
+    });
+
+    const referenceBitLength = Math.ceil(Math.log2(palette.length));
+    if (referenceBitLength === 0) {
+      throw new Error(
+        `The palette reference bit size is 0. This should not happen if at least one color is defined.`
+      );
+    }
+
+    let screenBuffer = Buffer.alloc(
+      Math.ceil((referenceBitLength * screen.length) / 8)
+    );
+    // The encoding of the screen buffer is a Little Endian bitwise encoding for
+    // each byte.
+    let bufferIndex = 0;
+    let current = 0;
+    let usedBits = 0;
+
+    screen.forEach((paletteIndex) => {
+      const reference = paletteIndex & (Math.pow(2, referenceBitLength) - 1);
+      const leftBits = 8 - usedBits;
+      const overflowBits = Math.max(0, referenceBitLength - leftBits);
+      current = current | ((reference << usedBits) & 0xff);
+      usedBits += referenceBitLength - overflowBits;
+      // console.log(
+      //   "reference",
+      //   reference,
+      //   "leftBits",
+      //   leftBits,
+      //   "overflowBits",
+      //   overflowBits,
+      //   "current",
+      //   current,
+      //   "usedBits",
+      //   usedBits
+      // );
+      if (usedBits === 8) {
+        // Add the byte and reset state
+        screenBuffer.writeUInt8(current, bufferIndex);
+        bufferIndex++;
+        current = 0;
+        usedBits = 0;
+      }
+      if (overflowBits > 0) {
+        // We had an overflow and need to preserve the overflow
+        current =
+          current |
+          ((reference >> (-1 * (overflowBits - referenceBitLength))) & 0xff);
+        usedBits = overflowBits;
+      }
+    });
+
+    // Add the last byte
+    if (usedBits != 0) {
+      screenBuffer.writeUInt8(current, bufferIndex);
+    }
+
+    const paletteSizeLE = Buffer.alloc(2);
+    if (palette.length === 65536) {
+      paletteSizeLE.writeUInt16LE(0);
+    } else {
+      paletteSizeLE.writeUInt16LE(palette.length);
+    }
+
+    const screenBufferSizeLE = Buffer.alloc(2);
+    screenBufferSizeLE.writeUInt16LE(screenBuffer.length);
+
+    const imageBuffer = Buffer.concat([paletteBuffer, screenBuffer]);
+    const imageBufferSizeLE = Buffer.alloc(2);
+    imageBufferSizeLE.writeUInt16LE(imageBuffer.length);
+
+    return {
+      paletteBuffer,
+      screenBuffer,
+      imageBuffer,
+      screen,
+      palette,
+      paletteSizeLE,
+      screenBufferSizeLE,
+      imageBufferSizeLE,
+    };
+  }
+}
